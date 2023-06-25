d76f78eb5f6469c20bc453a01dc490c59bb8614c
Umberto Mathias Iagushi Ferreira <umberto.mif@gmail.com>
2023-06-25 19:47:20 +0900

    Added the routes

--- upgrad-eshop/src/components/App.js
+++ upgrad-eshop/src/components/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './nav-bar/NavBar'
+import SignUp from './signup/SignUp';
+import SignIn from './signin/SignIn';
+import Home from './home/Home';
+import AddProduct from './add-product/AddProduct';
+import Products from './products/Products';
+import NoMatch from './no-match/NoMatch';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login/" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/add-product" element={<AddProduct />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="*" element={<NoMatch />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- upgrad-eshop/src/index.js
+++ upgrad-eshop/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Provider store={store}><App /></Provider>);
