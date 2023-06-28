9ff3771fdcec2f28c123056f31803295daeb53c4
Umberto Mathias Iagushi Ferreira <umberto.mif@gmail.com>
2023-06-28 11:08:16 +0900

    Updated App.js file.

--- upgrad-eshop/src/components/App.js
+++ upgrad-eshop/src/components/App.js
@@ -4,10 +4,11 @@ import SignUp from './signup/SignUp';
 import SignIn from './signin/SignIn';
 import Home from './home/Home';
 import AddProduct from './add-product/AddProduct';
+import UpdateProduct from './update-product/UpdateProduct';
 import Products from './products/Products';
 import NoMatch from './no-match/NoMatch';
 
-function App() {
+const App = () => {
     return (
         <Router>
             <NavBar />
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/login/" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/add-product" element={<AddProduct />} />
+                <Route path="/update-product/:id" element={<UpdateProduct />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="*" element={<NoMatch />} />
             </Routes>
